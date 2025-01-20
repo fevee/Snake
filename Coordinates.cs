97f33d6755e3eeee6d43ee8ad210894744f5f33e
Faye Vaquilar <fe.tvaquilar@gmail.com>
2025-01-20 10:44:05 -0600

    Initial commit

---
+++ Coordinates.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    internal class Coordinates
+    {
+        private int x;
+        private int y;
+
+        public int X { get { return x; }}
+
+        public int Y { get { return y; }}
+
+        public Coordinates(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
